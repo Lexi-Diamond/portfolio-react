f874ec328d0fb337e4ecb7ea3ce44dfed4bedc60
Lexi Dorrity <lexidorrity@Lexis-MBP.socal.rr.com>
2022-04-26 13:26:36 -0700

    added base code and file structure

---
+++ components/NavTabs.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+// TODO: Add a comment explaining how we are able to extract the key value pairs from props
+// desctructring currentPage and handle page change so we can extract key value pairs
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#home"
+          onClick={() => handlePageChange('Home')}
+          //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
+          // terinary operator if your on the home page add and activate nav link 
+          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          //  TODO: Add a comment explaining what this logic is doing
+          // if your on the about page its adding the class nav link active and not active for other nav links 
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#blog"
+          onClick={() => handlePageChange('Blog')}
+          //  TODO: Add a comment explaining what this logic is doing
+          // if your on the blog page activate the blog page adds class activate 
+          className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
+        >
+          Blog
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          //  TODO: Add a comment explaining what this logic is doing
+          // 
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
---
+++ components/PortfolioContainer.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // TODO: Add a comment describing the functionality of this method
+  // renders each page if the current page is home render home page etc
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Blog') {
+      return <Blog />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* // TODO: Add a comment describing what we are passing as props we are passing current page and handle page change*/}
+      
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* // TODO: Add a comment explaining what is happening on the following line calling above function to render the page */}
+      {renderPage()}
+    </div>
+  );
+}
